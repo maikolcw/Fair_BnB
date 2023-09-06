7c9e4592ef168643445b59e00f8a425a41852737
maikolcw <whysoraccoony@gmail.com>
2023-09-05 18:15:23 -0700

    updated font and global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// exposes classname for font
+const font = Nunito({ 
+  subsets: ['latin'], 
+});
 
 export const metadata: Metadata = {
   title: 'Fairbnb',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
