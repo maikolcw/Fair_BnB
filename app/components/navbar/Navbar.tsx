82dd6e8e6db429ffc484bd9376cb5337616459ca
maikolcw <whysoraccoony@gmail.com>
2023-09-06 17:41:41 -0700

    added navbar and container component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Container from '../Container'
 
 const Navbar = () => {
-  return (
-    <div>Navbar Component!</div>
-  )
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div
+                className="py-4 border-b-[1px]"
+            >
+                <Container>
+                    <div
+                        className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0"
+                    >
+
+                    </div>
+                </Container>
+            </div>
+
+        </div>
+    )
 }
 
 export default Navbar
