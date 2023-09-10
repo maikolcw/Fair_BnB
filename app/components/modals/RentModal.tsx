dfb5b24e86fe7ae49c853d2d0a72447a25bb604a
maikolcw <whysoraccoony@gmail.com>
2023-09-09 20:19:51 -0700

    added imageupload component for rentmodal and updated rentmodal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 enum STEPS {
     CATEGORY = 0,
@@ -170,8 +171,20 @@ const RentModal = () => {
         )
     }
 
-
-
+    if (step === STEPS.IMAGES) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Add a photo of your place"
+                    subtitle="Show guests what your place looks like!"
+                />
+                <ImageUpload
+                    onChange={(value) => setCustomValue('imageSrc', value)}
+                    value={imageSrc}
+                />
+            </div>
+        )
+    }
 
     return (
         <Modal
