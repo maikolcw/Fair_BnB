b8e9c00fe72017bdc9e4753fdca2183c32ba4a37
maikolcw <whysoraccoony@gmail.com>
2023-09-12 17:25:16 -0700

    added favorites pages and functions

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -107,7 +107,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                                 />
                                 <MenuItem
                                     label="My favorites"
-                                    onClick={() => { }}
+                                    onClick={() => router.push('/favorites')}
                                 />
                                 <MenuItem
                                     label="My reservations"
---
+++ app/favorites/page.tsx
@@ -0,0 +1,32 @@
+
+import EmptyState from "@/app/components/EmptyState";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getFavoriteListings from "@/app/actions/getFavoriteListings";
+
+import FavoritesClient from "./FavoritesClient";
+
+const ListingPage = async () => {
+    const listings = await getFavoriteListings();
+    const currentUser = await getCurrentUser();
+
+    if (listings.length === 0) {
+        return (
+            // client only
+            <EmptyState
+                title="No favorites found"
+                subtitle="Looks like you have no favorite listings."
+            />
+        );
+    }
+
+    return (
+        // client only
+        <FavoritesClient
+            listings={listings}
+            currentUser={currentUser}
+        />
+    );
+}
+
+export default ListingPage;
