854bf70fe025de7ddadd54e9ab53b66bbaaecba2
maikolcw <whysoraccoony@gmail.com>
2023-09-12 17:15:04 -0700

    added loginModal popup after registration, updated usermenu reservations

---
+++ app/reservations/page.tsx
@@ -0,0 +1,43 @@
+
+import EmptyState from "@/app/components/EmptyState";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getReservations from "@/app/actions/getReservations";
+
+import TripsClient from "./ReservationsClient";
+
+const ReservationsPage = async () => {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+        return (
+            // client only
+            <EmptyState
+                title="Unauthorized"
+                subtitle="Please login"
+            />
+        )
+    }
+
+    const reservations = await getReservations({ authorId: currentUser.id });
+
+    if (reservations.length === 0) {
+        return (
+            // client only
+            <EmptyState
+                title="No reservations found"
+                subtitle="Looks like you have no reservations on your properties."
+            />
+        );
+    }
+
+    return (
+        // client only
+        <TripsClient
+            reservations={reservations}
+            currentUser={currentUser}
+        />
+    );
+}
+
+export default ReservationsPage;
